9d8645872535a93e86cef0e561ba57da5a32cb5a
Christian Garcia <christiangorjon2003@gmail.com>
2023-11-29 20:39:37 +0100

    Añadida un par de funciones

--- src/Main.java
+++ src/Main.java
@@ -14,3 +14,24 @@ public class Saludo {
         System.out.println("¡Hola, " + nombre + "!");
     }
 }
+
+import java.util.ArrayList;
+        import java.util.List;
+        import java.util.stream.Collectors;
+        import java.util.stream.IntStream;
+
+public class NumeroPrimos {
+    public static void main(String[] args) {
+        List<Integer> numerosPrimos = crearNumerosPrimos(100);
+        System.out.println("Números primos hasta 100: " + numerosPrimos);
+    }
+
+    public static List<Integer> crearNumerosPrimos(int limite) {
+        return IntStream.rangeClosed(2, limite)
+                .filter(n -> {
+                    long sqrt = Math.sqrt(n);
+                    return n % sqrt == 0 || n % (sqrt - 1) == 0 ? false : true;
+                })
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+}
